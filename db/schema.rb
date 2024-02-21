79c125563fedbd8fd61991c4d9a10fa6a2123848
yamamotoknk <160210111+yamamotoknk@users.noreply.github.com>
2024-02-21 16:23:29 +0000

    [Finish] モデルとコントローラーの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2024_02_21_154532) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
