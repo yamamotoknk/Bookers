050ac59b593949aa20fcc7de51cf2ecc414a481c
yamamotoknk <160210111+yamamotoknk@users.noreply.github.com>
2024-02-27 13:43:26 +0000

    [Add]投稿詳細画面まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,53 @@
 class BooksController < ApplicationController
-  def new
+  
+  def create
+    	@book = Book.new(book_params)
+        if @book.save
+       	redirect_to book_path(@book.id)
+  	    else
+        @books = Book.all.order(id: "ASC")
+	    	render'books/index'
+  	    end
+  end
+  
+  def show
+    @book = Book.find(params[:id])  
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
   end
 
   def index
+    @books = Book.all
+
+    if params[:id].present?
+      set_book
+    else
+      @book = Book.new
+    end
   end
 
-  def show
+ def update
+    respond_to do |format|
+      if @book.update(book_params)
+        format.html { redirect_to request.referer, notice: 'Book was successfully updated.' }
+        format.json { render :show, status: :ok, location: @book }
+      else
+        format.html { render :edit }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
-  def edit
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
-Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get '/top' => 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  Rails.application.routes.draw do
+  root to: 'homes#top'
+  resources :books, expect: [:show, :new, :edit]
+	get 'books' =>'books#index'
+	post 'books' => 'books#create'
+	get 'books/:id' => 'books#show', as: 'book'
+	get 'books/:id/edit' => 'books#edit', as: 'edit_list'
+	patch 'books/:id' => 'books#update', as: 'update_book'
+	put 'books/:id' => 'books#update'
+	delete 'books/:id' => 'books#destroy'
+	end 
